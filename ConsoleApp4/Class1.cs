a3cd588a6d20e1509dbd946e4851d6fae21cab7d
Your Name <you@example.com>
2023-03-14 14:10:32 +0300

    Second

---
+++ ConsoleApp4/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Country
+    {
+        public string name { get; set; }
+        public string form { get; set; }
+        public float square { get; set; }
+
+    }
+}
